fd9c2e654d5420a8ff4c773574671024e40a2d2f
Joshua R. Engel <joshua@68studios.io>
2017-04-04 16:15:30 -0500

    Create QueryBuilder class

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
-// index.js
-//Does the lib-reg.json file exist?
-let _fs = require('fs');
-
-//Same source & path?
-let a, b;
-
-_fs.readdir('./', (err, files) => {
-  if (err) {
-    console.log("error arose: ", err);
-  }
-
-  a = files;
-  _fs.realpath(a[4], (err, resolvedPath) => {
-    if (err) {
-      console.log("error arose: ", err);
-    }
-    b = resolvedPath;
-    console.log("index.js file resolved to path of: ", resolvedPath);
-    //console.log(b == process.cwd()+'\\index.js' ? true : false);
-  });
-
-});
+// index.js - entry file for npm package.json
+module.exports = require('./app/main.js');
