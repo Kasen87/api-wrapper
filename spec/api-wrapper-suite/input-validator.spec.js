eb9e3957dfdcfd7c184943721b1f68f1d0ab690c
Joshua R. Engel <joshua@68studios.io>
2017-04-06 17:02:45 -0500

    Setup init specs for input-validator class

--- spec/api-wrapper-suite/input-validator.spec.js
+++ spec/api-wrapper-suite/input-validator.spec.js
@@ -7,6 +7,14 @@ const IV = require('../../app/helpers/input-validator.js');
  */
 
 describe('The InputValidator class', function(){
+  let _iv;
 
+  beforeEach(() => {
+    _iv = new IV();
+  });
+
+  it('should have a validateBaseURL method', () => {
+    expect(_iv.validateBaseURL).toBeDefined();
+  });
 
 });
