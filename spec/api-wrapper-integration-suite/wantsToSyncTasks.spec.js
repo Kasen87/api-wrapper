a61d109b1325bc39c193e6f7d67fcc4ddf113083
Joshua R. Engel <joshua@68studios.io>
2017-04-07 11:50:01 -0500

    Add wantsToSyncTasks.spec.js

---
+++ spec/api-wrapper-integration-suite/wantsToSyncTasks.spec.js
@@ -0,0 +1,15 @@
+/**
+ * User: A node developer
+ * Case: Wants to sync tasks from the Todoist API
+ * Issue: Never used this library before and is trusting
+ *        it to explicitly tell them what's messing up.
+ */
+
+//Using require index.js to bypass npm packing for ease
+describe('The API-Wrapper package', () => {
+  let _API;
+  it('should not throw errors when requiring the index.js file', () => {
+    _API = require('./index.js');
+  });
+});
+
