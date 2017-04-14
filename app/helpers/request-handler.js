1f72f6d7d78fe455a219991a3421f93ea75d072b
Joshua R. Engel <joshua@68studios.io>
2017-04-14 10:09:29 -0500

    Remove setupNock f(x) to a separate module

--- app/helpers/request-handler.js
+++ app/helpers/request-handler.js
@@ -5,37 +5,6 @@ const request = require('request');
 const rp_native = require('request-promise-native');
 const testEnv = require(process.cwd()+'/app/helpers/setupNock.js');
 
-function setupNockResponse(){
-  let _file = new Promise( (res, rej) => {
-    fs.readFile(todoistFile, 'utf8', (err, data) => {
-      if (err) {
-        rej(err);
-        return;
-      }
-      try {
-        //let _retData = JSON.parse(data);
-        res(data);
-      } catch(e) {
-        rej(e);
-        console.log(e.message);
-      }
-    });
-  });
-
-  let _nockResponse = {};
-
-  _file.then( function(res){
-    _nockResponse = res;
-  }, (err) => {
-    console.log("Setup Nock Error:", err);
-    _nockResponse = JSON.stringify(`{"Nock Setup Error": ${err}}`);
-  });
-
-  nock('https://todoist.com')
-    .post('/API/v7/sync')
-    .reply(200, _nockResponse);
-}
-
 class RequestHandler{
   constructor(props, isTest) {
     if(!props) throw new TypeError();
